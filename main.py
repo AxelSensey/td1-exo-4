5fdc249751814191738f6d0f99ed7773119d0baa
AxelSensey <68044018+AxelSensey@users.noreply.github.com>
2020-07-09 21:53:45 +1100

    Mise à jour pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-input.onButtonPressed(Button.A, function () {
-    if (x >= 0) {
+def on_button_pressed_a():
+    global x, y
+    if x >= 0:
         led.unplot(x, y)
         x += -1
         led.plot(x, y)
-    }
-    if (x < 0) {
+    if x < 0:
         led.unplot(x, y)
         x = 4
         y += -1
         led.plot(x, y)
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    if (x < 4) {
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global x, y
+    if x < 4:
         led.unplot(x, y)
         x += 1
         led.plot(x, y)
-    } else {
-        if (x >= 4) {
+    else:
+        if x >= 4:
             led.unplot(x, y)
             x = 0
             y += 1
             led.plot(x, y)
-        }
-    }
-})
-let y = 0
-let x = 0
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+y = 0
+x = 0
 x = 0
 y = 0
 led.plot(x, y)
